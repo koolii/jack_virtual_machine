8641fb79aa894876493d4d796c6dc2ba93aba2c1
koolii <kuriyama@createentrance.com>
2018-02-23 13:35:59 +0100

    :+1: [WIP] implement Parser

--- src/interface.ts
+++ src/interface.ts
@@ -3,16 +3,16 @@ export interface IParser {
   hasMoreCommands():void
   // 次のコマンドを読み込み、それを現コマンドとする
   // ここの結果がtrueの場合のみ、本ルーチンを呼ぶようにする
-  advance():void
+  advance(line: string):I_CMD
   // VMコマンドの種類を返す、算術コマンドは全てC_ARITHMETIC
-  commandType():void
+  commandType(operator: string):string
   // 現コマンドの最初の引数が返される
   // 算術コマンドの場合コマンド自体が帰る
   // C_RETURNの場合は、本ルーチンは呼ばない
-  arg1(row: string):void
+  arg1(operator: string):string
   // 現コマンドの２番めのひきすうがかえされる
   // C_PUSH/C_POP/C_FUNCTION/C_CALLの場合のみ本ルーチンを呼ぶようにする
-  arg2(row: string):number
+  arg2(operator: string):number
 }
 
 export interface ICodeWriter {
@@ -30,4 +30,11 @@ export interface ICodeWriter {
 export interface IM_FILE {
   path: string
   line: string[]
+}
+
+export interface I_CMD {
+  line: string
+  type: string
+  arg1: string|null
+  arg2: string|null
 }
--- src/parser.ts
+++ src/parser.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs-extra'
 import * as glob from 'glob'
 import { createInterface, ReadLine } from 'readline'
 import Logger from './logger'
-import { IParser, IM_FILE } from './interface'
+import { IParser, IM_FILE, I_CMD } from './interface'
 import constants from './constants'
 
 const ARG2_CMD: string[] = [
@@ -11,6 +11,7 @@ const ARG2_CMD: string[] = [
   constants.COMMAND.C_FUNCTION,
   constants.COMMAND.C_CALL,
 ]
+const CMD = constants.COMMAND
 
 export default class Parser implements IParser {
   logger: Logger
@@ -45,36 +46,81 @@ export default class Parser implements IParser {
       const readLine: ReadLine = createInterface({ input: stream })
 
       readLine
+        .on('close', () => { resolve({ path, line }) })
         .on('line', (l) => {
           // remove empty letter and comment part
           // 空白文字を削除しては行けない（この場所では）
+          // 複数スペースを削除する
+          // https://qiita.com/Yinaura/items/aa90473cb4a876e4988f
           // const fillOutEmpty = l.replace(/ /g, '')
           const hasCommentCharacter = l.match('//')
           const formatedLine = !hasCommentCharacter ? l : l.substring(0, hasCommentCharacter.index)
 
-          if (formatedLine !== '') {
-            line.push(formatedLine)
-          }
-        })
-        .on('close', () => {
-          resolve({
-            path,
-            line,
+          // セミコロンは別行として加算する
+          formatedLine.split(';').forEach((l: string) => {
+            if (l !== '') {
+              line.push(l)
+            }
           })
         })
     })
   }
 
   hasMoreCommands() {}
-  advance() {}
-  commandType() {}
-  arg1() {}
+
+  advance(line: string): I_CMD {
+    const opes = line.split(' ')
+
+    if (!opes[0]) {
+      throw new Error('line has no charactors. It it funny.')
+    }
+
+    const res = {
+      line,
+      type: this.commandType(opes[0]),
+      arg1: '', // なぜnullを渡すとエラーになる？interface側にもnullは許可しているのに
+      arg2: '',
+    }
+
+    if (opes[1]) {
+      res.arg1 = this.arg1(opes[1])
+    }
+    if (opes[2]) {
+      res.arg2 = this.arg1(opes[2])
+    }
+
+    return res
+  }
+
+  commandType(operator: string): string {
+    switch (operator) {
+      case 'push':
+        return CMD.C_PUSH
+      case 'pop':
+        return CMD.C_POP
+      case 'label':
+        return CMD.C_LABEL
+      case 'goto':
+        return CMD.C_GOTO
+      case 'if':
+        return CMD.C_IF
+      case 'function':
+        return CMD.C_FUNCTION
+      case 'return':
+        return CMD.C_RETURN
+      case 'call':
+        return CMD.C_CALL
+      default:
+        return CMD.C_ARITHMETIC
+    }
+  }
+
+  arg1(operator: string): string {
+    return ''
+  }
 
   // todo ここは只の一行の文字列だとコマンドをはんて出来ないのでModelを作成して渡す
-  arg2(row: string): number {
-    if (!ARG2_CMD.includes('')) {
-      throw new Error()
-    }
+  arg2(operator: string): number {
     return 0
   }
 }
